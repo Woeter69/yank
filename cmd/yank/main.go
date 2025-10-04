ee8411888b7d4a4cb539c38a67baaf92d9def7e4
Woeter <skippedlead@gmail.com>
2025-10-04 22:37:02 +0530

    Changed main.go

---
+++ cmd/yank/main.go
@@ -0,0 +1,15 @@
+package yank
+
+import (
+	"fmt"
+	"errors"
+	"github.com/spf13/cobra"
+	"github.com/Woeter69/yank/internal/install"
+	"github.com/Woeter69/yank/internal/utils"
+	"github.com/Woeter69/yank/internal/remove"
+
+)
+
+func main() {
+	fmt.Println("Will start soon!")
+}
