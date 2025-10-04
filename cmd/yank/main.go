c39410c4d13b2ef75071726362608b03c160df9f
Woeter <skippedlead@gmail.com>
2025-10-04 22:51:51 +0530

    Added cobra commands

--- cmd/yank/main.go
+++ cmd/yank/main.go
@@ -2,14 +2,28 @@ package yank
 
 import (
 	"fmt"
+	"os"
 	"errors"
 	"github.com/spf13/cobra"
 	"github.com/Woeter69/yank/internal/install"
 	"github.com/Woeter69/yank/internal/utils"
 	"github.com/Woeter69/yank/internal/remove"
-
 )
 
 func main() {
-	fmt.Println("Will start soon!")
+	rootCmd := &cobra.Command {
+		Use: "yank",
+		Short: "Yank package manager",
+	}
+
+	installCmd := &cobra.Command {
+		Use: "install [package]",
+		Short: "Install a package",
+	} 
+
+	removeCmd := &cobra.Command {
+		Use: "remove [package]",
+		Short: "Remove a package",
+	}
+
 }
